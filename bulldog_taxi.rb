e9d06ba8aaa3b4791b01c29b8bd7590c05491e98
Patrick Casey <patrick.casey@yale.edu>
2014-06-23 09:03:01 -0400

    v1

--- bulldog_taxi.rb
+++ bulldog_taxi.rb
@@ -6,18 +6,50 @@ configure do
 end
 
 before do
-	session['masterhash'] ||= {}
+	session['master_hash'] ||= {}
 end
+
+# before do
+# 	session['most_recent'] ||= {}
+# end
 
 get '/' do
-	test = "testing"
-	erb :index, :locals => { :test => test }
+	erb :index
 end
 
-get '/confirmation' do
-	test = "testing"
-	erb :confirmation, :locals => { :test => test }
+get '/index/confirmation' do
+	erb :confirmation, locals: { master_hash: session['master_hash'] }
 end
+
+post '/' do
+  rider_name = params[:name].to_s
+  # session['master_array'].push rider_name
+  # most_recent = { 'name' => params[:name].to_s,
+  #                 'email' => params[:email].to_s,
+  #                 'arrival_time' => params[:arrival_time] }
+  session['master_hash'][rider_name] = { 'name' => params[:name].to_s,
+                                     		 'email' => params[:email].to_s,
+                                     		 'arrival_time' => params[:arrival_time] }
+  erb :confirmation, locals: { master_hash: session['master_hash'] }
+  # master_array = master_hash.to_a
+  # most_recent = master_array.length
+  redirect '/index/confirmation'
+end
+
+# get '/index/confirmation' do
+# 	confirm = session['master_hash'][params[:most_recent]]['name']
+# 	erb :confirmation, locals: { confirm: confirm }
+# end
+
+delete '/index/confirmation/:rider' do
+  name = params[:rider]
+  session['master_hash'].delete(name)
+  erb :confirmation, locals: { master_hash: session['master_hash'] }
+end
+
+# get '/index/confirmation' do
+# 	erb :confirmation
+# end
 
 # get '/confirmation' do
 # 	session["masterhash"] ||= {}
